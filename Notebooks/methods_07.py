0d6da7c213611d38ce0f6dcc3fb4afc78d81e9a9
r-doz <romina.ma95@gmail.com>
2024-05-08 12:24:50 +0200

    solutions of notebook 7

--- Notebooks/methods_07.py
+++ Notebooks/methods_07.py
@@ -7,13 +7,20 @@ def conditioned_scale_file(obs, guess=8.5):
     return measurement
 
 def eight_school_file(J, sigma, y=None):
-    #to do: implement the model
-    return
+    mu = pyro.sample('mu', dist.Normal(0, 5))
+    tau = pyro.sample('tau', dist.HalfCauchy(5))
+    with pyro.plate('J', J):
+        theta = pyro.sample('theta', dist.Normal(mu, tau))
+        pyro.sample('obs', dist.Normal(theta, sigma), obs=y)
 
 
 def eight_schools_noncentered_file(J, sigma, y=None):
-    #to do: implement the model
-    return
+    mu = pyro.sample('mu', dist.Normal(0, 5))
+    tau = pyro.sample('tau', dist.HalfCauchy(5))
+    with pyro.plate('J', J):
+        nu = pyro.sample('nu', dist.Normal(0., 1.))
+        theta = mu + tau * nu
+        pyro.sample('obs', dist.Normal(theta, sigma), obs=y)
 
 
 def hmm_file(weather_seq, clothes_seq, n_weather_cat, transition_prior, emission_prior, n_data_observations):
@@ -26,5 +33,11 @@ def hmm_file(weather_seq, clothes_seq, n_weather_cat, transition_prior, emission
     weather_cat = weather_seq[0] 
     
     # evolve in time
-    #to do
-    return
+    for t in range(n_data_observations):
+        if t > 0:
+            weather_cat = pyro.sample("weather_{}".format(t), 
+                                   dist.Categorical(transition_prob[weather_cat]),
+                                   obs=weather_seq[t])
+        pyro.sample("clothes_{}".format(t), 
+                    dist.Categorical(emission_prob[weather_cat]), 
+                    obs=clothes_seq[t])
